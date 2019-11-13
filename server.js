00410c3c684ad9c70523d7cf980943eb71b180e1
= <=>
2019-11-13 10:08:58 -0600

    Agregando uso de CORS

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+var cors = require('cors')
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3002;
-	app.listen(port);
-	app.use('/api/registro_participantes', require('./api/routes/routes'));
-	console.log('todo list RESTful API server started on: ' + port);
+app.use(cors());
+app.listen(port);
+app.use('/api/registro_participantes', require('./api/routes/routes'));
+console.log('todo list RESTful API server started on: ' + port);
